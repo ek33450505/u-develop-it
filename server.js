1fa8c4b2b1fe56173603ca776aeb96759bdd9c01
Ed Kubiak <ek33450505@gmail.com>
2022-04-19 13:48:25 -0400

    add CRUD routes

--- server.js
+++ server.js
@@ -22,37 +22,84 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
-// Displays a list of all potential candidates
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
-// GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    // params is in addition because we are requiring the id number for this api to work
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
 
 // Delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(result);
-//   });
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+  // params is in addition because we are requiring the id number for this api to work
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) { // this is added for when a user trys to delete a candidate that doesnt exist
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        // this will verify whether any rows were changed
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
 
-// Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// Create a candidate POST will insert a candidate back into the table
+app.post('/api/candidate', ({ body }, res) => {
+    // Use inputCheck to validate the user daya before changes are inserted into the db, keep free of erroneous data
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
 
 db.query(sql, params, (err, result) => {
   if (err) {
-    console.log(err);
+    res.status(400).json({ error: err.message });
+    return;
   }
-  console.log(result);
+  res.json({
+    message: 'success',
+    data: body
+  });
+});
 });
 
 // Default response for any other request (Not Found)
